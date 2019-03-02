8f2d86d75fc3c4dd00002aa7564b13693bec8117
Marc-Antoine Ruel <hello@marcantoineruel.com>
2019-03-02 15:30:53 -0500

    Optimize createMediaNode function

--- src/normalize/createMediaNode.js
+++ src/normalize/createMediaNode.js
@@ -1,85 +1,38 @@
 import extensionAttributes from './extensionAttributes'
 
-export default function(createNodeId, entities) {
-  // image
-  // media_gallery_entries
-  // small_image
-  // thumbnail
-  // swatch_image
-
+export default function(entities) {
   let medias = []
 
-  const addToArray = (baseURL, path) => {
-    const sourceUrl = `${baseURL}catalog/product${path}`
-    const id = createNodeId(`magento__media-${path}`)
-
-    if (
-      medias.findIndex(({ file_path: filePath }) => filePath === path) === -1
-    ) {
-      const obj = {
-        id,
-        source_url: sourceUrl,
-        file_path: path,
-        parent: null,
-        children: [],
-        __type: 'magento__media',
-      }
-
-      medias = [...medias, obj]
-    }
-    return id
-  }
-
   entities = entities.map(cur => {
-    if (cur.__type === 'magento__product') {
-      const baseURL = entities.find(e => e.id === cur.storeViewConfig___NODE)
-        .secure_base_media_url
+    if (
+      cur.__type === 'magento__product' &&
+      cur.media_gallery_entries &&
+      cur.media_gallery_entries.length > 0
+    ) {
+      const {
+        secure_base_media_url: baseURL,
+        id: storeViewConfigID,
+        code: storeViewConfigCode,
+      } = entities.find(e => e.id === cur.storeViewConfig___NODE)
 
-      if (cur.image) {
-        const id = addToArray(baseURL, cur.image)
+      cur.media_gallery_entries = extensionAttributes(cur.media_gallery_entries)
 
-        cur.image___NODE = id
+      cur.media_gallery_entries.forEach(e => {
+        const sourceUrl = `${baseURL}catalog/product${e.file}`
 
-        delete cur.image
-      }
+        const obj = {
+          ...e,
+          magento_id: e.id,
+          source_url: sourceUrl,
+          storeViewConfig___NODE: storeViewConfigID,
+          storeViewConfigCode,
+          parent: null,
+          children: [],
+          __type: 'magento__media',
+        }
 
-      if (cur.small_image) {
-        const id = addToArray(baseURL, cur.small_image)
-
-        cur.small_image___NODE = id
-
-        delete cur.small_image
-      }
-
-      if (cur.thumbnail) {
-        const id = addToArray(baseURL, cur.thumbnail)
-
-        cur.thumbnail___NODE = id
-
-        delete cur.thumbnail
-      }
-
-      if (cur.swatch_image) {
-        const id = addToArray(baseURL, cur.swatch_image)
-
-        cur.swatch_image___NODE = id
-
-        delete cur.swatch_image
-      }
-
-      if (cur.media_gallery_entries && cur.media_gallery_entries.length > 0) {
-        cur.media_gallery_entries = extensionAttributes(
-          cur.media_gallery_entries,
-        )
-
-        cur.media_gallery_entries.forEach(e => {
-          const id = addToArray(baseURL, e.file)
-
-          e.file___NODE = id
-
-          delete e.file
-        })
-      }
+        medias = [...medias, obj]
+      })
     }
 
     return cur
