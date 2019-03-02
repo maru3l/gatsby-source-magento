63413d021fc88fbd1898f5133c269622fbd8127a
Marc-Antoine Ruel <hello@marcantoineruel.com>
2019-03-02 15:30:22 -0500

    create mapProductConfigurationOptions function

---
+++ src/normalize/mapProductConfigurationOptions.js
@@ -0,0 +1,56 @@
+export default function(entities) {
+  const attributes = entities.filter(
+    ({ __type }) => __type === 'magento__productAttributes',
+  )
+
+  const products = entities.filter(
+    ({ __type }) => __type === 'magento__product',
+  )
+  return entities.map(entity => {
+    if (
+      entity.__type === 'magento__product' &&
+      entity.configurable_product_options
+    ) {
+      const {
+        storeViewConfigCode,
+        configurable_product___NODE: configurableProductNode,
+      } = entity
+
+      const configurableProduct = configurableProductNode.map(id =>
+        products.find(({ id: pID }) => pID === id),
+      )
+
+      entity.configurable_product_options = entity.configurable_product_options.map(
+        option => {
+          const {
+            options: attributeOptions,
+            attribute_code: attributeCode,
+          } = attributes.find(
+            attr =>
+              attr.storeViewConfigCode === storeViewConfigCode &&
+              Number(attr.magento_id) === Number(option.attribute_id),
+          )
+
+          option.values = option.values.map(({ value_index: index }) => {
+            const label = attributeOptions.find(
+              ({ value }) => Number(index) === Number(value),
+            )
+
+            const product = configurableProduct.find(
+              ({ [attributeCode]: code }) => Number(code) === Number(index),
+            )
+
+            return {
+              ...label,
+              product___NODE: product.id,
+            }
+          })
+
+          return option
+        },
+      )
+    }
+
+    return entity
+  })
+}
