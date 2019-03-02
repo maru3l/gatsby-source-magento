9b5774f28f5a0cec9de339175181642cee46ed86
Marc-Antoine Ruel <hello@marcantoineruel.com>
2019-03-02 15:29:17 -0500

    add storeViewConfiguration to categories

--- src/normalize/mapProductsWithCategories.js
+++ src/normalize/mapProductsWithCategories.js
@@ -1,11 +1,14 @@
 export default function(entities) {
   return entities.map(e => {
     if (e.category_links) {
+      const { storeViewConfigCode } = e
+
       e.category_links = e.category_links.map(
         ({ category_id: id, ...link }) => {
           link.category___NODE = entities.find(
             entity =>
               Number(entity.magento_id) === Number(id) &&
+              entity.storeViewConfigCode === storeViewConfigCode &&
               entity.__type === 'magento__category',
           ).id
 
