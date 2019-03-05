b1e3f703a496989db1854617d5c1037333e4bb9c
Marc-Antoine Ruel <hello@marcantoineruel.com>
2019-03-05 15:27:11 -0500

    add caching to download media

--- src/normalize/downloadMedias.js
+++ src/normalize/downloadMedias.js
@@ -3,26 +3,48 @@ const { createRemoteFileNode } = require(`gatsby-source-filesystem`)
 export default async (nodes, store, cache, createNode, createNodeId) =>
   Promise.all(
     nodes.map(async node => {
-      if (node.__type === 'magento__media') {
-        let fileNode
+      let fileNodeID
+      if (node.__type === `magento__media`) {
+        const mediaDataCacheKey = `magento-media-${node.wordpress_id}`
+        const cacheMediaData = await cache.get(mediaDataCacheKey)
 
-        try {
-          fileNode = await createRemoteFileNode({
-            url: node.source_url,
-            store,
-            cache,
-            createNode,
-            createNodeId,
-          })
-        } catch (error) {
-          console.error(error)
+        // If we have cached media data and it wasn't modified, reuse
+        // previously created file node to not try to redownload
+        if (cacheMediaData && node.modified === cacheMediaData.modified) {
+          fileNodeID = cacheMediaData.fileNodeID
+          touchNode({ nodeId: cacheMediaData.fileNodeID })
         }
 
-        if (fileNode) {
-          node.localFile___NODE = fileNode.id
+        // If we don't have cached data, download the file
+        if (!fileNodeID) {
+          try {
+            const fileNode = await createRemoteFileNode({
+              url: node.source_url,
+              store,
+              cache,
+              createNode,
+              createNodeId,
+              parentNodeId: node.id,
+            })
 
-          delete node.file
+            if (fileNode) {
+              fileNodeID = fileNodnode.id
+
+              await cache.set(mediaDataCacheKey, {
+                fileNodeID,
+                modified: node.modified,
+              })
+            }
+          } catch (e) {
+            // Ignore
+          }
         }
+      }
+
+      if (fileNodeID) {
+        node.localFile___NODE = fileNodeID
+
+        delete node.file
       }
 
       return node
