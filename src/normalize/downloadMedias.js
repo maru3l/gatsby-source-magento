07053bc3385d7f481961df56beeecded7d2133c7
Marc-Antoine Ruel <hello@marcantoineruel.com>
2019-03-22 13:42:10 -0400

    remove custom caching

--- src/normalize/downloadMedias.js
+++ src/normalize/downloadMedias.js
@@ -1,50 +1,43 @@
 const { createRemoteFileNode } = require(`gatsby-source-filesystem`)
 
-export default async (
-  nodes,
-  store,
-  cache,
-  createNode,
-  createNodeId,
-  touchNode,
-) =>
+export default async (nodes, store, cache, createNode, createNodeId) =>
   Promise.all(
     nodes.map(async node => {
       let fileNodeID
       if (node.__type === `magento__media`) {
-        const mediaDataCacheKey = `magento-media-${node.magento_id}`
-        const cacheMediaData = await cache.get(mediaDataCacheKey)
+        // const mediaDataCacheKey = `magento-media-${node.magento_id}`
+        // const cacheMediaData = await cache.get(mediaDataCacheKey)
 
         // If we have cached media data and it wasn't modified, reuse
         // previously created file node to not try to redownload
-        if (cacheMediaData) {
-          fileNodeID = cacheMediaData.fileNodeID
-          touchNode({ nodeId: cacheMediaData.fileNodeID })
-        }
+        // if (cacheMediaData) {
+        //   fileNodeID = cacheMediaData.fileNodeID
+        //   touchNode({ nodeId: cacheMediaData.fileNodeID })
+        // }
 
         // If we don't have cached data, download the file
-        if (!fileNodeID) {
-          try {
-            const fileNode = await createRemoteFileNode({
-              url: node.source_url,
-              store,
-              cache,
-              createNode,
-              createNodeId,
-              parentNodeId: node.id,
-            })
+        // if (!fileNodeID) {
+        try {
+          const fileNode = await createRemoteFileNode({
+            url: node.source_url,
+            store,
+            cache,
+            createNode,
+            createNodeId,
+            parentNodeId: node.id,
+          })
 
-            if (fileNode) {
-              fileNodeID = fileNode.id
+          if (fileNode) {
+            fileNodeID = fileNode.id
 
-              await cache.set(mediaDataCacheKey, {
-                fileNodeID,
-              })
-            }
-          } catch (e) {
-            console.error(e)
+            // await cache.set(mediaDataCacheKey, {
+            //   fileNodeID,
+            // })
           }
+        } catch (e) {
+          console.error(e)
         }
+        // }
       }
 
       if (fileNodeID) {
