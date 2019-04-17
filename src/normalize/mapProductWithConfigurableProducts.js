58b8667fae95c71dcf0fc373e5bd82d3658538f6
Marc-Antoine Ruel <hello@marcantoineruel.com>
2019-04-17 13:16:25 -0400

    add langage key in productconfigurationlink

--- src/normalize/mapProductWithConfigurableProducts.js
+++ src/normalize/mapProductWithConfigurableProducts.js
@@ -1,11 +1,14 @@
 export default function(entities) {
   return entities.map(e => {
     if (e.configurable_product_links) {
+      const { storeViewConfigCode } = e
+
       e.configurable_product___NODE = e.configurable_product_links.map(
         id =>
           entities.find(
             el =>
               Number(el.magento_id) === Number(id) &&
+              el.storeViewConfigCode === storeViewConfigCode &&
               el.__type === 'magento__product',
           ).id,
       )
